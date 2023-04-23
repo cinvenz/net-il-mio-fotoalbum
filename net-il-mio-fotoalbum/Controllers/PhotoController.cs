3932fd8af725ebb23b683219ed3763064fd90c1c
cinvenz <vincenzobortune@gmail.com>
2023-04-23 11:43:23 +0200

    insert Update and Delete

--- net-il-mio-fotoalbum/Controllers/PhotoController.cs
+++ net-il-mio-fotoalbum/Controllers/PhotoController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using net_il_mio_fotoalbum.Models;
+using System.Data;
 using System.Diagnostics;
 using static System.Net.Mime.MediaTypeNames;
 using static System.Net.WebRequestMethods;
@@ -30,7 +32,7 @@ namespace net_il_mio_fotoalbum.Controllers
         {
             var photos = _context.Photos.SingleOrDefault(p => p.Id == id);
 
-			if (photos == null) 
+			if (photos is null) 
 			{ 
 				return NotFound($"Photo with id {id} not found.");	
 			}
@@ -62,5 +64,67 @@ namespace net_il_mio_fotoalbum.Controllers
 			return RedirectToAction("index");
 		}
 
-	}
+        public IActionResult Update(int id)
+        {
+
+          
+            var photo = _context.Photos.FirstOrDefault(p => p.Id == id);
+
+			if(photo is null)
+			{
+				return View("NotFound");
+			}
+          
+			return View(photo);	
+        }
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Update(int id, Photo photo)
+		{
+			if (!ModelState.IsValid) 
+			{ 
+				return View(photo); 
+			}
+			var photoToUpdate = _context.Photos.FirstOrDefault(p => p.Id == id);
+
+			if (photoToUpdate is null)
+			{
+				return View("NotFound");
+			}
+
+			photoToUpdate.Title = photo.Title;
+			photoToUpdate.Description = photo.Description;
+			photoToUpdate.ImageFile = photo.ImageFile;
+			photoToUpdate.Visible = photo.Visible;
+
+			_context.SaveChanges();
+			return RedirectToAction("index");	
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Delete(int id)
+		{
+			var photoToDelete = _context.Photos.FirstOrDefault(p => p.Id == id);
+
+			if (photoToDelete is null)
+			{
+				return View("NotFound");
+			}
+
+			_context.Photos.Remove(photoToDelete);
+			_context.SaveChanges();
+
+			return RedirectToAction("Index");
+		}
+
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+    }
 }
---
+++ net-il-mio-fotoalbum/Views/Photo/Update.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace net_il_mio_fotoalbum.Views.Photo
+{
+    public class UpdateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
